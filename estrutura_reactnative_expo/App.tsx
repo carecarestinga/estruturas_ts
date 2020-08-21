c25ac37acdf4ddf8c2b2d003d21d3e09cc282fe9
Felipe Becker Nunes <35842441+nunesfb@users.noreply.github.com>
2020-08-21 12:16:50 -0300

    estrutura_reactnative_expo

---
+++ estrutura_reactnative_expo/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// Estou criando um Function Component vazio e passo uma view
+// Assim, a tela que vai ser mostrada é toda em branco
+const App: React.FC = () => (
+  <View>
+    <Text>Hello World!</Text>
+  </View>
+);
+
+export default App;
